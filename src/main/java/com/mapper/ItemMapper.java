56ebab93e25b7e3e4cd83554c4e6e14b5a4af33a
abodziak <agnieszka.bodziak@igt.com>
2025-06-18 13:12:52 +0200

    toList

--- src/main/java/com/mapper/ItemMapper.java
+++ src/main/java/com/mapper/ItemMapper.java
@@ -4,11 +4,10 @@ import com.dto.ItemDto;
 import com.entities.Item;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ItemMapper {
     public List<ItemDto> mapItemToItemDto(List<Item> items) {
-        return items.stream().map(item -> new ItemDto(item.getItemId(), item.getName())).collect(Collectors.toList());
+        return items.stream().map(item -> new ItemDto(item.getItemId(), item.getName())).toList();
     }
 
     public ItemDto mapItemToItemDto(Item item) {
